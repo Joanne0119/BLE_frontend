6eb6debd4c1d8e13ccab3cbe9ead6680f955375d
劉丞恩 <114967562+Joanne0119@users.noreply.github.com>
2025-07-25 14:54:38 +0800

    fix css media query

--- src/App.vue
+++ src/App.vue
@@ -31,9 +31,10 @@ header {
 
 nav {
   width: 100%;
-  font-size: 12px;
+  font-size: 1rem; 
   text-align: center;
-  margin-top: 2rem;
+  margin-top: 1rem; 
+  padding: 1rem 0;
 }
 
 nav a.router-link-exact-active {
@@ -52,32 +53,5 @@ nav a {
 
 nav a:first-of-type {
   border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
